20d05fba6fc296f87247267e8c72fa6912dc0a77
everlastingQAQ <2279839810@qq.com>
2026-07-26 19:37:46 +0800

    Finish lab2

--- lab2/DebugExercise/DebugExercise1.java
+++ lab2/DebugExercise/DebugExercise1.java
@@ -6,7 +6,7 @@ package DebugExercise;
  */
 public class DebugExercise1 {
     public static long divideThenRound(int top, int bottom) {
-        double quotient = top / bottom;
+        double quotient = (double)top / bottom;
         long result = Math.round(quotient);
         return result;
     }
